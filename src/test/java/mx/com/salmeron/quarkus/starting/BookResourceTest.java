a837e0918db0354737ab3893ea6c8b9dcef48c6b
Sergio Salmeron <sergio@salmeron.com.mx>
2021-08-22 11:46:24 -0500

    Added Tests

--- src/test/java/mx/com/salmeron/quarkus/starting/BookResourceTest.java
+++ src/test/java/mx/com/salmeron/quarkus/starting/BookResourceTest.java
@@ -1,7 +1,11 @@
 package mx.com.salmeron.quarkus.starting;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -10,12 +14,45 @@ import static org.hamcrest.CoreMatchers.is;
 public class BookResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    @DisplayName("testHelloEndpoint API")
+    public void testgetAllBooks() {
         given()
-          .when().get("/api/books")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .accept(MediaType.APPLICATION_JSON)
+                .when().get("/api/books")
+                .then()
+                .statusCode(200)
+                .body("size()", is(2));
     }
 
+
+    @Test
+    @DisplayName("count All Books Test")
+    public void countAllBooksTest() {
+        given()
+                .accept(MediaType.TEXT_PLAIN)
+                .when()
+                .get("/api/books/count")
+                .then()
+                .statusCode(200)
+                .body(is("2"));
+
+    }
+
+    @Test
+    @DisplayName("Get a Specific Book Test")
+    public void getABookTest() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)  //Difference way of achieve
+                //.accept(MediaType.APPLICATION_JSON)
+                .pathParam("id", 1)
+                .when()
+                .get("/api/books/{id}")
+                .then()
+                .statusCode(200)
+                .body("title", is("Understanding Quarkus"))
+                .body("author", is("Antonio Goncalves"))
+                .body("yearOfPublication", is(2020))
+                .body("genre", is("IT"));
+
+    }
 }
